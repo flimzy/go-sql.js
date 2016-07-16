afbbffffec25643ebbeb8c7d4a253f1e9b105933
Jonathan Hall <flimzy@flimzy.com>
2016-07-16 17:47:26 +0200

    Play nicely with standard browser loading of sql.js, too

--- bindings/sqljs.inc.js
+++ bindings/sqljs.inc.js
@@ -1,1 +1,7 @@
-$global.SQL = require('sql.js');
+if ( typeof($global.SQL) === 'undefined' ) {
+    try {
+        $global.SQL = require('sql.js');
+    } catch(e) {
+        throw("Cannot find global SQL object. Did you load sql.js?");
+    }
+}
